81e4cef94765b9bf90f50024667c0e3e24bd31cb
박창건 <79077488+changgun-park@users.noreply.github.com>
2023-01-25 04:09:37 +0900

    Time: 91 ms (43.15%), Space: 48.5 MB (47.72%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let index = 0;
+    let result = null;
+    
+    const dfs = (node) => {
+        if (!node) return;
+        dfs(node.left);
+        if (++index === k) result = node.val;
+        dfs(node.right);
+    }
+
+    dfs(root)
+    
+    return result
+    
+};
